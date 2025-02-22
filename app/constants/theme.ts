d0044f9949dfc82e34609a6b1811d5776e4f1732
angelah1020 <angela.huang1020@gmail.com>
2025-02-22 04:48:47 -0500

    Change color theme to blue :)

--- app/constants/theme.ts
+++ app/constants/theme.ts
@@ -1,30 +1,105 @@
 import { MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
 
+// Define your custom colors
+const customColors = {
+  primary: '#0D2F5D',    // Dark blue
+  secondary: '#78A8D9',  // Medium blue
+  tertiary: '#CADEED',   // Light blue
+  darkBlue: '#092442',   // Darker version of primary
+  lightBlue: '#E5F0F7',  // Lighter version of tertiary
+  background: '#F8FBFD',
+};
+
 export const lightTheme = {
   ...MD3LightTheme,
   colors: {
-    ...MD3LightTheme.colors,
-    primary: '#6750A4',
-    secondary: '#625B71',
-    tertiary: '#7D5260',
-    background: '#FFFBFE',
-    surface: '#FFFBFE',
-    error: '#B3261E',
+    // Override all MD3 colors with our blue palette
+    primary: customColors.primary,
+    onPrimary: '#FFFFFF',
+    primaryContainer: customColors.secondary,
+    onPrimaryContainer: customColors.primary,
+    secondary: customColors.secondary,
+    onSecondary: customColors.primary,
+    secondaryContainer: customColors.tertiary,
+    onSecondaryContainer: customColors.primary,
+    tertiary: customColors.tertiary,
+    onTertiary: customColors.primary,
+    tertiaryContainer: customColors.lightBlue,
+    onTertiaryContainer: customColors.primary,
+    background: customColors.background,
+    onBackground: customColors.primary,
+    surface: customColors.lightBlue,
+    onSurface: customColors.primary,
+    surfaceVariant: customColors.tertiary,
+    onSurfaceVariant: customColors.secondary,
+    surfaceDisabled: customColors.tertiary,
+    onSurfaceDisabled: customColors.secondary,
+    outline: customColors.secondary,
+    outlineVariant: customColors.tertiary,
+    shadow: customColors.darkBlue,
+    scrim: customColors.darkBlue,
+    inverseSurface: customColors.primary,
+    inverseOnSurface: customColors.tertiary,
+    inversePrimary: customColors.tertiary,
+    error: customColors.darkBlue,
+    onError: customColors.tertiary,
+    errorContainer: customColors.primary,
+    onErrorContainer: customColors.tertiary,
+    elevation: {
+      level0: 0,
+      level1: 1,
+      level2: 2,
+      level3: 3,
+      level4: 4,
+      level5: 5,
+    },
   },
 };
 
 export const darkTheme = {
   ...MD3DarkTheme,
   colors: {
-    ...MD3DarkTheme.colors,
-    primary: '#D0BCFF',
-    secondary: '#CCC2DC',
-    tertiary: '#EFB8C8',
-    background: '#1C1B1F',
-    surface: '#1C1B1F',
-    error: '#F2B8B5',
+    // Override all MD3 colors with our blue palette
+    primary: customColors.tertiary,
+    onPrimary: customColors.primary,
+    primaryContainer: customColors.secondary,
+    onPrimaryContainer: customColors.tertiary,
+    secondary: customColors.secondary,
+    onSecondary: customColors.tertiary,
+    secondaryContainer: customColors.primary,
+    onSecondaryContainer: customColors.tertiary,
+    tertiary: customColors.primary,
+    onTertiary: customColors.tertiary,
+    tertiaryContainer: customColors.darkBlue,
+    onTertiaryContainer: customColors.tertiary,
+    background: customColors.darkBlue,
+    onBackground: customColors.tertiary,
+    surface: customColors.primary,
+    onSurface: customColors.tertiary,
+    surfaceVariant: customColors.secondary,
+    onSurfaceVariant: customColors.tertiary,
+    surfaceDisabled: customColors.darkBlue,
+    onSurfaceDisabled: customColors.secondary,
+    outline: customColors.secondary,
+    outlineVariant: customColors.tertiary,
+    shadow: customColors.darkBlue,
+    scrim: customColors.darkBlue,
+    inverseSurface: customColors.tertiary,
+    inverseOnSurface: customColors.primary,
+    inversePrimary: customColors.primary,
+    error: customColors.tertiary,
+    onError: customColors.primary,
+    errorContainer: customColors.secondary,
+    onErrorContainer: customColors.primary,
+    elevation: {
+      level0: 0,
+      level1: 1,
+      level2: 2,
+      level3: 3,
+      level4: 4,
+      level5: 5,
+    },
   },
 };
 
-// Dummy default export to satisfy the warning
 export default { lightTheme, darkTheme };
